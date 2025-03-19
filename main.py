ac1aec5d0aff3e238a635ab52a058dc5138f819d
kalmary <58998639+kalmary@users.noreply.github.com>
2025-03-19 15:29:57 +0100

    update

--- main.py
+++ main.py
@@ -45,6 +45,9 @@ class CorrosionData:
         @property
         def data(self):
                 return self._data
+        @property
+        def data_numeric(self):
+                return self._data._get_numeric_data()
 
         def _convert2numeric(self) -> pd.DataFrame:
                 self._data.iloc[:, 1:] = self._data.iloc[:, 1:].apply(pd.to_numeric, errors='raise')
@@ -103,31 +106,81 @@ class CorrosionData:
                 data_avg = data_avg[
                         (data_avg['Date-Time'] >= first_valid_time) & (data_avg['Date-Time'] <= last_valid_time)]
 
-                data_avg['Date-Time'] = self._data['Date-Time'].dt.strftime('%H:%M')
-                data_avg['Date-Time'] = data_avg['Date-Time'].dt.hour
-                data_avg = data_avg.groupby(data_avg['Date-Time'])
+                # data_avg['Date-Time'] = self._data['Date-Time'].dt.strftime('%H:%M')
+
+                # Dodajemy kolumnę godzinową
+                data_avg['Date-Time'] = data_avg[time_name].dt.hour # time albo hour
 
 
+                # Grupowanie po godzinach
+                data_avg = data_avg.groupby('Date-Time').mean()
 
 
+                data_avg = data_avg.rename(columns={'Date-Time': 'Hour'})
 
-        def plot_parameters(self, data, x_param_name: str, y_params: list[str] = None) -> None:
+                data_avg = data_avg.drop(columns = ['Test Time (h)'])
+
+                return data_avg
+
+        def plot_Trend(self, x: np.array, y: np.array, poly_order: int = 3) -> object:
+                x, y = self.sort_cols(x, y)
+                # Fit a linear trend line (1st-degree polynomial)
+                coeffs = np.polyfit(x, y, poly_order)  # Linear fit
+                trend_line = np.poly1d(coeffs)
+
+                return trend_line
+
+        def sort_cols(self, x, y) -> tuple[np.array, np.array]:
+                x = np.asarray(x)
+                y = np.asarray(y)
+
+                sorted_indices = np.argsort(x)  # Get sorted indices of the first row
+                x = x[sorted_indices]
+                y = y[sorted_indices]
+
+                x = x.tolist()
+                y = y.tolist()
+
+                return x, y
+        def make_lists(self, x, y):
+                x = np.asarray(x)
+                y = np.asarray(y)
+
+                x = x.tolist()
+                y = y.tolist()
+
+                return x, y
+
+
+        def plot_parameters(self, x_param_name: str, y_params: list[str] = None, trend_line: bool = True, sort_x: bool = True) -> None:
                 """Plot selected parameters as a function of the chosen parameter."""
-
 
                 if y_params is None:
                         # If no specific parameters are provided, plot all columns except the chosen one
                         y_params = [col for col in self.column_names if col != x_param_name]
 
-                column_x, x_label = self._getColumn(x_param_name)
+                column_x = self._data[x_param_name]
+                x_label = x_param_name
 
 
                 plt.figure(figsize=(10, 6))
 
                 # Plot each of the parameters against the chosen parameter
                 for y_param in y_params:
-                        column_y, y_label = self._getColumn(y_param)
-                        plt.plot(column_x, column_y, label = y_label)
+                        column_y = self._data[y_param]
+                        y_label = y_param
+                        if sort_x is True:
+                                column_x, column_y = self.sort_cols(column_x, column_y)
+                        else:
+                                column_x, column_y = self.make_lists(column_x, column_y)
+
+                        plt.scatter(column_x, column_y, label = y_label, marker = 'o')
+                        if trend_line is True:
+                                try:
+                                        trend_eq = self.plot_Trend(column_x, column_y)
+                                        plt.plot(column_x, trend_eq(column_x), linestyle= '-.', label = f'Trend Line of {y_label}')
+                                except Exception as e:
+                                        print(f'Error when computing trend line: {e}.\nThis behavior is normal for irregular data')
 
 
                 plt.xlabel(x_label)
@@ -151,47 +204,59 @@ class Analyzer:
                 plt.show()
 
                 return correlation_matrix
-        
-def main():
-        path = 'Acuity_LS_00833_20250226_102627.csv'
-        data_obj = CorrosionData(path)
+
+def loadRawData(path):
+        data_obj = CorrosionData(path=path)
         data_obj.loadData()
         data_obj.add_NewColumn(column2apply='Galv Corr 1 (A)',
                                new_column_name='Galv Corr Mass Loss Rate (g/m-a)',
-                               func=lambda x: x*data_obj.coeffs['galvanic_corrosion'])
+                               func=lambda x: x * data_obj.coeffs['galvanic_corrosion'])
 
         data_obj.add_NewColumn(column2apply='Galv Corr 2 (A)',
                                new_column_name='Free Corr Mass Loss Rate (g/m-a)',
-                               func=lambda x: x*data_obj.coeffs['galvanic_corrosion'])
+                               func=lambda x: x * data_obj.coeffs['galvanic_corrosion'])
 
         data_obj.add_NewColumn(column2apply='Tot Galv Corr 1 (C)',
                                new_column_name='Tot Galv Corr Mass Loss Rate (g/m-a)',
-                               func=lambda x: x*data_obj.coeffs['galvanic_corrosion'])
+                               func=lambda x: x * data_obj.coeffs['galvanic_corrosion'])
 
         data_obj.add_NewColumn(column2apply='Tot Galv Corr 2 (C)',
                                new_column_name='Tot Free Corr Mass Loss Rate (g/m-a)2',
-                               func=lambda x: x*data_obj.coeffs['free_corrosion'])
+                               func=lambda x: x * data_obj.coeffs['free_corrosion'])
 
         print('--- COLUMN NAMES ---\n')
         for i, name in enumerate(data_obj.column_names):
                 print(f'index: {i}, name: {name}')
         print(data_obj.column_names)
 
-        data_obj.Compute_1dAverages('Date-Time')
+        return data_obj
 
 
+def main():
+        path = 'Acuity_LS_00833_20250226_102627.csv'
+        data_obj = loadRawData(path)
+
+        data_avg = data_obj.Compute_1dAverages()
         data_avg = data_obj.Compute_DailyAverages()
+
         data_obj_avg = CorrosionData(data=data_avg)
 
-        data_selected = data_obj_avg.select_byColumnNames(['Air Temp (C)', 'RH (%)', 'Surface Temp (C)',
-                                                           'Cond Lo Freq (S)', 'Cond Hi Freq (S)',
-                                                           'Galv Corr 1 (A)', 'Galv Corr 2 (A)',
-                                                           'Tot Cond Lo Freq (C/V)', 'Tot Cond Hi Freq (C/V)',
-                                                           'Tot Galv Corr 1 (C)', 'Tot Galv Corr 2 (C)',
-                                                           'Galv Corr Mass Loss Rate (g/m-a)', 'Free Corr Mass Loss Rate (g/m-a)',
-                                                           'Tot Galv Corr Mass Loss Rate (g/m-a)', 'Tot Free Corr Mass Loss Rate (g/m-a)2'])
+        # data_selected = data_obj_avg.select_byColumnNames(['Air Temp (C)', 'RH (%)', 'Surface Temp (C)',
+        #                                                    'Cond Lo Freq (S)', 'Cond Hi Freq (S)',
+        #                                                    'Galv Corr 1 (A)', 'Galv Corr 2 (A)',
+        #                                                    'Tot Cond Lo Freq (C/V)', 'Tot Cond Hi Freq (C/V)',
+        #                                                    'Tot Galv Corr 1 (C)', 'Tot Galv Corr 2 (C)',
+        #                                                    'Galv Corr Mass Loss Rate (g/m-a)', 'Free Corr Mass Loss Rate (g/m-a)',
+        #                                                    'Tot Galv Corr Mass Loss Rate (g/m-a)', 'Tot Free Corr Mass Loss Rate (g/m-a)2'])
+        print(data_obj_avg.column_names)
+        data_obj_avg.plot_parameters(x_param_name = 'Air Temp (C)', y_params=['RH (%)'],
+                                     trend_line=True)
 
-        analyzer_obj = Analyzer(data_selected)
+        analyzer_obj = Analyzer(data_obj_avg.select_byColumnNames(['Air Temp (C)', 'RH (%)', 'Surface Temp (C)',
+                                                                   'Tot Cond Lo Freq (C/V)', 'Tot Cond Hi Freq (C/V)',
+                                                                   'Tot Galv Corr 1 (C)', 'Tot Galv Corr 2 (C)',
+                                                                   'Galv Corr Mass Loss Rate (g/m-a)', 'Free Corr Mass Loss Rate (g/m-a)',
+                                                                   'Tot Galv Corr Mass Loss Rate (g/m-a)', 'Tot Free Corr Mass Loss Rate (g/m-a)2']))
         cor = analyzer_obj.find_correlations()
 
 
